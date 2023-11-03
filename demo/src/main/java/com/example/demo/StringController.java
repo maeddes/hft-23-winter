e7169df2a14886d7795e7dceed0cfbb5dfccca73
maeddes <maeddes@gmail.com>
2023-11-03 12:07:04 +0000

    Lightweight JS Frontend

--- demo/src/main/java/com/example/demo/StringController.java
+++ demo/src/main/java/com/example/demo/StringController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/strings")
+@CrossOrigin(origins = "*") // DON'T DO THIS :-)
 public class StringController {
 
     private List<String> stringList = new ArrayList<>();
